1bbafe2a2b67c856d12f42d7e53128f5125a06e5
GuofengBai <GuofengBai@yeah.net>
2018-03-26 13:10:03 +0800

    初始化gitlab-ci项目。

---
+++ DevOpsGitlabCI/src/main/java/com/guofengbai/DevOpsGitlabCI/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.guofengbai.DevOpsGitlabCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
