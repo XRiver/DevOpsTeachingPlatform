f9ed1c2364e1a159194092e9b3b3be47283c692b
bufapiqi <26716594@qq.com>
2018-03-28 15:00:08 +0800

    CreateDockerProject
    DockerProject with SpringBoot

---
+++ DevOpsDocker/DockerBranch/src/main/java/Devops/docker/DockerBranch/DockerBranchApplication.java
@@ -0,0 +1,12 @@
+package Devops.docker.DockerBranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBranchApplication.class, args);
+	}
+}
