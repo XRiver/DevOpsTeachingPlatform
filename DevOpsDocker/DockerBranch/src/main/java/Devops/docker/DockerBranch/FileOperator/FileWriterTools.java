52d53c7568b95607d94ec0339e09edffc01b8e57
bufapiqi <26716594@qq.com>
2018-04-09 17:23:21 +0800

    更新一一下javadoc

--- DevOpsDocker/DockerBranch/src/main/java/Devops/docker/DockerBranch/FileOperator/FileWriterTools.java
+++ DevOpsDocker/DockerBranch/src/main/java/Devops/docker/DockerBranch/FileOperator/FileWriterTools.java
@@ -11,6 +11,15 @@ package Devops.docker.DockerBranch.FileOperator;
 public abstract class FileWriterTools {
 	
 	
+	/**
+	 * 写文件里面的内容
+	 * @param Path 文件的路径
+	 * @param FileName 文件的名字
+	 * @param FileType 文件的类型（后缀）
+	 * @param containt 要写的文件内容
+	 * @return boolean True写成功，false写失败
+	 * 
+	 * */
 	public abstract boolean WriteFile(String Path,String FileName,String FileType,StringBuilder containt);
 
 
