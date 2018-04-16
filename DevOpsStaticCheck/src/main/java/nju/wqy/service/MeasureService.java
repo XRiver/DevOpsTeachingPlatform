8e56b2c5ac43bfb1c3f83a094b9b54cbad861d65
iDimple <wqy14@software.nju.edu.cn>
2018-04-16 20:59:50 +0800

    一些度量值

---
+++ DevOpsStaticCheck/src/main/java/nju/wqy/service/MeasureService.java
@@ -0,0 +1,7 @@
+package nju.wqy.service;
+
+import nju.wqy.web.vo.MeasureVO;
+
+public interface MeasureService {
+MeasureVO getMeasure(String projectKey);
+}
---
+++ DevOpsStaticCheck/src/main/java/nju/wqy/service/impl/MeasureServiceImpl.java
@@ -0,0 +1,97 @@
+package nju.wqy.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import nju.wqy.service.MeasureService;
+import nju.wqy.util.APIManager;
+import nju.wqy.web.vo.IndexVO;
+import nju.wqy.web.vo.MeasureVO;
+@Service
+public class MeasureServiceImpl implements MeasureService{
+
+	@Override
+	public MeasureVO getMeasure(String projectKey) {
+		//得到各种问题的数量
+		String result=APIManager.get("http://localhost:9000/api/measures/component?additionalFields=periods&componentKey="+projectKey+"&metricKeys=bugs%2Cclasses%2Ccode_smells%2Ccomment_lines%2Ccomment_lines_density%2Cvulnerabilities%2Cfunctions%2Cfiles%2Clines%2Cncloc");
+		MeasureVO vo=null;
+		if(result!=null){  
+			Gson gs = new Gson();  
+			System.out.println(result);
+			JSONObject obj=JSONObject.fromObject(result);      
+			obj=obj.getJSONObject("component"); 
+			JSONArray arr=obj.getJSONArray("measures");
+			vo=getVo(arr);
+		}
+		return vo;
+	}
+	public static MeasureVO getVo(JSONArray arr) {
+		MeasureVO vo=new MeasureVO();
+		for(int i=0;i<arr.size();i++) {
+			Object o=arr.get(i);
+			String metric=getValue(o,"metric");
+			switch(metric){
+			case "comment_lines":
+				//注释行
+				vo.setCommentsLines(getIntegerValue(o));
+				break;
+			case "lines":
+				//总行数
+				vo.setLine(getIntegerValue(o));
+				break;
+			case "comment_lines_density":
+				//注释比
+				vo.setCommentRate(getDoubleValue(o));
+				break;
+			case "classes":
+				//类
+				vo.setClasses(getIntegerValue(o));
+				break;	
+			case "files":
+				//文件
+				vo.setFile(getIntegerValue(o));
+				break;
+			case "functions":
+				//方法
+				vo.setFunctions(getIntegerValue(o));
+				break;
+			case "ncloc":
+				//逻辑代码
+				vo.setLoc(getIntegerValue(o));
+				break;
+			case "vulnerabilities":
+				vo.setVulnerability(getIntegerValue(o));
+				break;
+			case "code_smells":
+				vo.setCodeSmell(getIntegerValue(o));
+				break;
+			case "bugs":
+				vo.setBug(getIntegerValue(o));
+				break;
+			}
+		}
+		return vo;
+	}
+	public static int getIntegerValue(Object o) {	
+		return Integer.parseInt(getValue(o));
+	}
+	public static double getDoubleValue(Object o) {	
+		return Double.parseDouble(getValue(o));
+	}
+	public static String getValue(Object o) {
+		System.out.println(o);
+		JSONObject obj=JSONObject.fromObject(o); 	
+		String value=obj.getString("value");
+		System.out.println(value);
+		return value;
+	}
+	public static String getValue(Object o,String value) {
+
+		JSONObject obj=JSONObject.fromObject(o); 	
+		return obj.getString(value);
+
+	}
+}
