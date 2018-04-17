452378c874ab59b0129b489ee291ab31cf70667e
csy14 <141250005@smail.nju.edu.cn>
2018-04-17 21:07:25 +0800

    。。
    。。

---
+++ DevOpsTeamwork/src/main/java/module/group/DAO/Impl/GroupDaoImpl.java
@@ -0,0 +1,27 @@
+package module.group.DAO.Impl;
+
+import module.entity.PO.GroupPO;
+import module.group.DAO.GroupDao;
+
+/**
+ * Created by caosh on 2018/4/12.
+ */
+
+public class GroupDaoImpl implements GroupDao {
+
+    public boolean addGroup(GroupPO groupPO) {
+        return false;
+    }
+
+    public boolean deleteGroup(int groupId) {
+        return false;
+    }
+
+    public boolean modifyGroup(GroupPO groupPO) {
+        return false;
+    }
+
+    public GroupPO getGroup(int groupId) {
+        return null;
+    }
+}
