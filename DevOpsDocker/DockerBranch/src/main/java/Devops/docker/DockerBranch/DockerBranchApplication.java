9b05fc35e3858fa88da69a7751d2dd493014489a
bufapiqi <26716594@qq.com>
2018-05-23 15:33:21 +0800

    ss

--- DevOpsDocker/DockerBranch/src/main/java/Devops/docker/DockerBranch/DockerBranchApplication.java
+++ DevOpsDocker/DockerBranch/src/main/java/Devops/docker/DockerBranch/DockerBranchApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 import javax.servlet.MultipartConfigElement;
@@ -29,6 +32,17 @@ public class DockerBranchApplication {
 		/// 设置总上传数据总大小
 		factory.setMaxRequestSize("100MB");
 		return factory.createMultipartConfig();
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// TODO Auto-generated method stub
+				registry.addMapping("/**").allowedOrigins("http://localhost:8887");
+			}
+		};
 	}
 
 }
